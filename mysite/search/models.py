e1f17be0a11c680c5a9633a8ce0fbbe4a544995f
Moritz <moritz.mueller2@tu-dresden.de>
2021-04-07 14:50:16 +0200

    database modeled by inspectdb

--- mysite/search/models.py
+++ mysite/search/models.py
@@ -1,3 +1,29 @@
+# Create your models here.
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-# Create your models here.
+
+class Files(models.Model):
+    md5_filepath = models.TextField(db_column='MD5_FilePath', blank=True, null=True)  # Field name made lowercase.
+    currenttime = models.TextField(db_column='CurrentTime', blank=True, null=True)  # Field name made lowercase.
+    filename = models.TextField(db_column='FileName', blank=True, null=True)  # Field name made lowercase.
+    fileextension = models.TextField(db_column='FileExtension', blank=True, null=True)  # Field name made lowercase.
+    filetype = models.TextField(db_column='FileType', blank=True, null=True)  # Field name made lowercase.
+    filefullpath = models.TextField(db_column='FileFullPath', unique=True, blank=True, null=True)  # Field name made lowercase.
+    filefullpathplaceholder = models.TextField(db_column='FileFullPathPlaceholder', blank=True, null=True)  # Field name made lowercase.
+    directorypath = models.TextField(db_column='DirectoryPath', blank=True, null=True)  # Field name made lowercase.
+    filesize = models.IntegerField(db_column='FileSize', blank=True, null=True)  # Field name made lowercase.
+    filecreationdate = models.TextField(db_column='FileCreationDate', blank=True, null=True)  # Field name made lowercase.
+    filelastmodificationdate = models.TextField(db_column='FileLastModificationDate', blank=True, null=True)  # Field name made lowercase.
+    fileowner = models.TextField(db_column='FileOwner', blank=True, null=True)  # Field name made lowercase.
+    key = models.TextField(db_column='Key', primary_key=True, blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'Files'
