426725c0b0b0107df00278b1b552b4f5f2acc7ff
Moritz <moritz.mueller2@tu-dresden.de>
2021-04-13 11:12:00 +0200

    session based, sorting and pagination fully works now

--- mysite/search/views.py
+++ mysite/search/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponseRedirect
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.urls import reverse
 from django.views.generic import ListView
@@ -9,7 +10,13 @@ from .forms import SearchForm
 from .tables import FilesTable
 
 def results(request):
-    if request.method == 'POST':        
+    if request.method == 'POST':
+        #check cookie from previous view
+        #if request.session.test_cookie_worked():
+        #    request.session.delete_test_cookie()
+        #else:
+        #    return HttpResponse("Please enable cookies and try again.")
+
         # Create a form instance and populate it with data from the request (binding):
         form = SearchForm(request.POST)
 
@@ -20,27 +27,30 @@ def results(request):
             min_size = int(form.cleaned_data['min_size'])
             max_size = int(form.cleaned_data['max_size'])
             case_sensitive = form.cleaned_data['case_sensitive'] #not used
-            # context = {
-            #     'entries': 
-            # }
+            
+            request.session['pattern'] = pattern
+            request.session['min_size'] = min_size
+            request.session['max_size'] = max_size
+            request.session['case_sensitive'] = case_sensitive
 
             table = FilesTable(Files.objects.filter(filefullpath__icontains=pattern, filesize__gte=min_size, filesize__lte=max_size))
     #if GET
     else:
-        #if session, take values from it
+        #get the form data from the session
+        try:
+            pattern = request.session['pattern']
+            min_size = request.session['min_size']
+            max_size = request.session['max_size']
+            case_sensitive = request.session['case_sensitive']
+        except KeyError:
+            return HttpResponse('Please enable cookies.')
         table = FilesTable(Files.objects.filter(filefullpath__icontains=pattern, filesize__gte=min_size, filesize__lte=max_size))
-        #table = FilesTable(Files.objects.all())
-
-    RequestConfig(request, paginate={"per_page": 25}).configure(table)
-    return render(request, 'search/results.html', {"table": table })
-
-    # template = loader.get_template('search/index.html')
-    # context = {
-    #     'filtered_entries': Files.objects.all()[:5],
-    # }
-    # return HttpResponse(template.render(context, request))
+        
+    RequestConfig(request, paginate={'per_page': 25}).configure(table)
+    return render(request, 'search/results.html', {'table': table })
 
 def search(request):
+    #request.session.set_test_cookie()
     form = SearchForm()
 
     context = {
