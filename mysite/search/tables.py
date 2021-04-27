7638e1ca9f01cc57b9dc41b6373b090b60c07acb
Moritz <moritz.mueller2@tu-dresden.de>
2021-04-27 11:11:08 +0200

    changed column size

--- mysite/search/tables.py
+++ mysite/search/tables.py
@@ -3,9 +3,14 @@ from .models import Files
 
 class FilesTable(tables.Table):  
     filetype = tables.Column(orderable=False)
-    filefullpath = tables.Column(attrs={"td": {"onClick": "navigator.clipboard.writeText(this.innerText); alert('Copied file path to clipboard!');"}})
+    #filefullpath = tables.Column(attrs={"td": {"onClick": "navigator.clipboard.writeText(this.innerText); alert('Copied file path to clipboard!');"}})
     #bootstrap responsive columns
     #filepath = tables.Column(orderable=False, attrs={"td": {"onClick": "navigator.clipboard.writeText(this.innerText); alert('File path is in Clipboard!');",, "class":"filecolumn" }})  CSS: .filecolumn{ width: 75%}
+    #filefullpath = tables.Column(attrs={"td": {"onClick": "navigator.clipboard.writeText(this.innerText); alert('Copied file path to clipboard!');", "class":"col-8" }})
+    filefullpath = tables.Column(attrs={"td": {"onClick": "navigator.clipboard.writeText(this.innerText); alert('Copied file path to clipboard!');", "width":"70%" }})
+    #filetype = tables.Column(attrs={"td": {"class":"col" }})
+    #filesize = tables.Column(attrs={"td": {"class":"col" }})
+    #filelastmodificationdate = tables.Column(attrs={"td": {"class":"col" }})
     class Meta:
         model = Files        
         fields = ("filefullpath", "filetype", "filesize", "filelastmodificationdate")
