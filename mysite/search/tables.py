4f252a60ace25ccee75e5c76b1e46cc5a7a355c8
Moritz <moritz.mueller2@tu-dresden.de>
2021-04-30 17:41:31 +0200

    changed font size in table

--- mysite/search/tables.py
+++ mysite/search/tables.py
@@ -7,7 +7,7 @@ class FilesTable(tables.Table):
     #bootstrap responsive columns
     #filepath = tables.Column(orderable=False, attrs={"td": {"onClick": "navigator.clipboard.writeText(this.innerText); alert('File path is in Clipboard!');",, "class":"filecolumn" }})  CSS: .filecolumn{ width: 75%}
     #filefullpath = tables.Column(attrs={"td": {"onClick": "navigator.clipboard.writeText(this.innerText); alert('Copied file path to clipboard!');", "class":"col-8" }})
-    filefullpath = tables.Column(attrs={"td": {"onClick": "navigator.clipboard.writeText(this.innerText); alert('Copied file path to clipboard!');", "width":"70%" }})
+    filefullpath = tables.Column(attrs={"td": {"onClick": "navigator.clipboard.writeText(this.innerText); alert('Copied file path to clipboard!');", "width":"65%" }})
     #filetype = tables.Column(attrs={"td": {"class":"col" }})
     #filesize = tables.Column(attrs={"td": {"class":"col" }})
     #filelastmodificationdate = tables.Column(attrs={"td": {"class":"col" }})
